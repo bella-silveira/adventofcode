8fc85245e876c7c8166c8d0fa7d4dcdaf0a58232
Isabella Silveira <isabella.silveira.souza@gmail.com>
2020-12-01 11:31:46 +0100

    [day 01] second puzzle (although I'm not proud of this solution)

---
+++ 2020/01/puzzle2.js
@@ -0,0 +1,15 @@
+const input = require("./input")
+
+const findNumbers = () => {
+  for (let i = 0; i < input.length; i++) {
+    for (let j = 0; j < input.length; j++) {
+      for (let k = 0; k < input.length; k++) {
+        if (input[i] + input[j] + input[k] === 2020) {
+          return input[i] * input[j] * input[k]
+        }
+      }
+    }
+  }
+}
+
+console.log(findNumbers())
