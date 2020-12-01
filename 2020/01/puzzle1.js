3990e6f51894c73aab644376487c852042d38325
Isabella Silveira <isabella.silveira.souza@gmail.com>
2020-12-01 14:16:13 +0100

    [day 01] improve implementation on puzzle 1

--- 2020/01/puzzle1.js
+++ 2020/01/puzzle1.js
@@ -1,13 +1,23 @@
 const input = require("./input")
 
 const findNumbers = () => {
-  for (let i = 0; i < input.length; i++) {
-    for (let j = 0; j < input.length; j++) {
-      if (input[i] + input[j] === 2020) {
-        return input[i] * input[j]
-      }
+  const sumTarget = 2020
+  const sortedInput = input.sort((a, b) => a - b)
+  let left = 0
+  let right = sortedInput.length - 1
+  let sum = 0
+
+  while (sum !== sumTarget) {
+    sum = sortedInput[left] + sortedInput[right]
+
+    if (sum < sumTarget) {
+      left++
+    } else if (sum > sumTarget) {
+      right--
     }
   }
+
+  return sortedInput[left] * sortedInput[right]
 }
 
 console.log(findNumbers())
