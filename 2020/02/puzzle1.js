9dcfc51fff2d54760ea1679957ae01e78002ee98
Isabella Silveira <isabella.silveira.souza@gmail.com>
2020-12-02 10:43:54 +0100

    [day 02] add comment to solution of puzzle 1

--- 2020/02/puzzle1.js
+++ 2020/02/puzzle1.js
@@ -9,7 +9,12 @@ const calculateValidPasswords = () => {
       const minMax = item[0].split("-")
       const letter = item[1].slice(0, 1)
       const password = item[2]
-      
+
+      // haha very creative JS problem solving, _but_ essentially 
+      // I'm using string.split to tell me how many occurrences of a letter there are in the string
+      // e.g. if there aren't "f" in the "abcde" string, it'll return the same string in in array (["abcde"]) of length 1
+      // whereas, in "fdefdefde", it'll return an array with 4 elements (whatever is in between the Fs)
+      // so we know that the number of occurrences will always be the length of the split result - 1
       if ((password.split(letter).length - 1) >= parseInt(minMax[0]) && (password.split(letter).length - 1) <= parseInt(minMax[1])) {
         validPasswods.push(password)
       }
