2e0e5b404d53d21864420c880e44d0718cd747c4
Isabella Silveira <isabella.silveira.souza@gmail.com>
2020-12-10 10:50:36 +0100

    [day 10] improve const name in puzzle 2

--- 2020/10/puzzle2.js
+++ 2020/10/puzzle2.js
@@ -7,9 +7,9 @@ const findJoltPermutation = () => {
   }
 
   for(let i = 1; i < sortedAdapters.length; i++) {
-    const combinations = (permutations[sortedAdapters[i] - 1] || 0) + (permutations[sortedAdapters[i] - 2] || 0) +(permutations[sortedAdapters[i] - 3] || 0)
+    const permutationSum = (permutations[sortedAdapters[i] - 1] || 0) + (permutations[sortedAdapters[i] - 2] || 0) + (permutations[sortedAdapters[i] - 3] || 0)
     
-    permutations[sortedAdapters[i]] = combinations
+    permutations[sortedAdapters[i]] = permutationSum
   }
 
  return permutations[input[input.length - 1]]
